fbc9a1c006e7f33e4c64e55c6b0064fccce20a15
Vadim Belov <git@belov.us>
2026-06-19 01:40:06 +0000

    feat: add android share target entry

--- src/Cotton.Mobile/Platforms/Android/MainActivity.cs
+++ src/Cotton.Mobile/Platforms/Android/MainActivity.cs
@@ -1,21 +1,35 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.Content.Res;
 using Android.OS;
+using Android.Util;
 using Android.Views;
 using Cotton.Mobile.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Cotton.Mobile
 {
-    [Activity(Theme = "@style/Cotton.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Theme = "@style/Cotton.SplashTheme", MainLauncher = true, Exported = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [IntentFilter(new[] { Intent.ActionSend }, Categories = new[] { Intent.CategoryDefault }, DataMimeType = "*/*")]
+    [IntentFilter(new[] { Intent.ActionSendMultiple }, Categories = new[] { Intent.CategoryDefault }, DataMimeType = "*/*")]
     public class MainActivity : MauiAppCompatActivity
     {
+        private const string ShareIntentLogTag = "CottonShare";
+
         protected override void OnCreate(Bundle? savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             ApplySystemBars();
+            DeferShareIntent(Intent);
+        }
+
+        protected override void OnNewIntent(Intent? intent)
+        {
+            base.OnNewIntent(intent);
+
+            DeferShareIntent(intent);
         }
 
         protected override void OnResume()
@@ -34,6 +48,20 @@ namespace Cotton.Mobile
             base.OnConfigurationChanged(newConfig);
 
             ApplySystemBars();
+        }
+
+        private static void DeferShareIntent(Intent? intent)
+        {
+            if (intent?.Action is not Intent.ActionSend and not Intent.ActionSendMultiple)
+            {
+                return;
+            }
+
+            string action = intent.Action ?? "unknown";
+            string mimeType = intent.Type ?? "unknown";
+            Log.Info(
+                ShareIntentLogTag,
+                $"Deferred Android share intent until Capture Inbox is available. Action={action}; MimeType={mimeType}.");
         }
 
         private void ApplySystemBars()

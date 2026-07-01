79c232d7463e9736dd8c308626afa784e02de5d0
Vadim Belov <git@belov.us>
2026-07-01 10:05:35 +0000

    style: align android text selection accent

--- src/Cotton.Mobile/MauiBuilderDesignSystemExtensions.cs
+++ src/Cotton.Mobile/MauiBuilderDesignSystemExtensions.cs
@@ -2,6 +2,7 @@
 // Copyright (c) 2025–2026 Vadim Belov <https://belov.us>
 
 #if ANDROID
+using Android.Content;
 using Android.Content.Res;
 using Microsoft.Maui.Handlers;
 #endif
@@ -17,7 +18,22 @@ namespace Cotton.Mobile
 				nameof(UseCottonDesignSystem),
 				static (handler, view) =>
 				{
+					Context context = handler.PlatformView.Context
+						?? throw new InvalidOperationException("Entry context was not found.");
+					Resources resources = context.Resources
+						?? throw new InvalidOperationException("Entry resources were not found.");
+
 					handler.PlatformView.BackgroundTintList = ColorStateList.ValueOf(Android.Graphics.Color.Transparent);
+					handler.PlatformView.SetHighlightColor(
+						resources.GetColor(Resource.Color.cotton_text_highlight, context.Theme));
+
+					if (OperatingSystem.IsAndroidVersionAtLeast(29))
+					{
+						handler.PlatformView.SetTextCursorDrawable(Resource.Drawable.cotton_text_cursor);
+						handler.PlatformView.SetTextSelectHandle(Resource.Drawable.cotton_text_select_handle);
+						handler.PlatformView.SetTextSelectHandleLeft(Resource.Drawable.cotton_text_select_handle);
+						handler.PlatformView.SetTextSelectHandleRight(Resource.Drawable.cotton_text_select_handle);
+					}
 				});
 #endif
 			return builder;
